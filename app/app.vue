27e69840f8790e5295c402c399ede1d939b1edce
Francesco Miranda <framira2005@gmail.com>
2026-05-26 16:55:11 +0200

    feat: initial commit - Nuxt and Tauri setup

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen bg-neutral-50 text-neutral-900">
+    <div class="text-center">
+      <h1 class="text-4xl font-extrabold tracking-tight mb-2">Verna</h1>
+    </div>
+  </div>
+</template>
